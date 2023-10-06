879916cc9c8930a0e7396fad8f416ce10e0fbd93
tanushahande20 <tanushahande2003@gmail.com>
2023-10-06 17:21:22 +0530

    first commit

---
+++ app/src/main/java/com/example/womens_safety_app/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.womens_safety_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+import com.example.womens_safety_app.MainActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new CountDownTimer(1000,500){
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                SplashScreen.this.finish();
+            }
+        }.start();
+
+    }
+}
